cb516f0e78f8469e938672741807a8c345de8289
Hernan Garcia <hernangarcia706@gmail.com>
2022-05-03 01:17:07 -0300

    Prueba proyecto final

--- src/Componentes/Cart.jsx
+++ src/Componentes/Cart.jsx
@@ -5,14 +5,16 @@ import Form from "react-bootstrap/Form"
 import Table from "react-bootstrap/Table"
 import Container from "react-bootstrap/Container"
 import {CartContext} from "../Context/CartContext"
-import { useState, useEffect,useContext } from "react";
+import { useState,useContext } from "react";
 import {collection,addDoc, getFirestore} from "firebase/firestore"
+import { useForm } from 'react-hook-form'
 
 
 
 export  default function Cart() {
 
     const {cart,removeCart,buyAll,valorTotal,cantidadTotal} = useContext(CartContext)
+    // const {register, errors, handleSubmit} = useForm();
 
     const [name, setName] =   useState();
     const [email, setEmail] = useState();
@@ -37,83 +39,101 @@ export  default function Cart() {
 
     alert("Tu pedido se ha registrado" + "\n"+ name +"\n"+ email +"\n" +phone);
     }
+    const handleClick= () => {
+        terminarCompra()
+        buyAll()
+        
+    }
+    const onSubmit = (data) => {
+        console.log(data)
+    }
 
-    // useEffect(() => {
-    //     console.log(name, email, phone);
-    //     }, [name, email, phone]);
-    
-    
-    
     return(
         <>
-      <Container className="div-nombre">
-            <h1 className="h1-nombre">CARRITO</h1>
+      <Container className="contenedor-inputs">
+
+        <p className="texto-carrito">CARRITO</p>
+        
+            <Table className="table"  bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                        
+                        <th>Cantidad</th>
+                        <th>Nombre del auto</th>
+                        <th>Precio</th>
+                        <th>Total</th>
+                        <th>Borrar</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                        cart.length > 0 ? cart.map (item =>
+                        <tr>
+                        <td>{item.cantidad}</td>
+                        <td>{item.nombre}</td>
+                        <td>{item.precio} €</td>
+                        <td>{item.cantidad * parseInt(item.precio)} € </td>
+                        <td><Button variant="outline-danger" onClick={() => removeCart(item)} >X</Button></td>
+                        </tr>)
+                        :
+                        <tr>
+                        <td>vacio</td>
+                        <td>vacio</td>
+                        <td>vacio</td>
+                        <td>vacio</td>
+                        <td><Button variant="outline-danger" disabled >X</Button></td>
+                        </tr>
+                        }
+                    </tbody>
+                    <thead>
+                        <tr>
+                        
+                        <th>{cantidadTotal()}</th>
+                        <th>-</th>
+                        <th>Total</th>
+                        <th>{valorTotal()} €</th>
+                        <th>-</th>
+                        
+                        </tr>
+                    </thead>
+            </Table>
+      
+        <p className="texto-datos">Ingresa tus datos para finalizar la compra</p>
+
+            <Form className="form-contacto">
+                <ul>
+                    <li>
+                        <input 
+                        placeholder={"Nombre y apellido"}  
+                        type={"text"} 
+                        value={name}
+                        name={"nombre"}
+                        onChange={(e) => {setName(e.currentTarget.value);}}/>
+                    </li>
+                    <li>
+                        <input 
+                        placeholder={"E-mail"} 
+                        type={"email"} 
+                        value={email}
+                        onChange={(e) => {setEmail(e.currentTarget.value);}}/>
+                    </li>
+                    <li>
+                        <input 
+                        placeholder={"Numero de Celular"} 
+                        type={"number"} 
+                        value={phone}
+                        onChange={(e) => {setPhone(e.currentTarget.value);}}/>
+                    </li>
+                    <li>
+                    <Button variant="outline-success" onClick={handleClick}>Finalizar Comprar</Button>
+                    </li>
+                </ul>
+            </Form>
+
       </Container>
-      <Container>  
-       <Table striped bordered hover variant="dark">
-            <thead>
-                <tr>
-                
-                <th>Cantidad</th>
-                <th>Nombre del auto</th>
-                <th>Precio</th>
-                <th>Total</th>
-                <th>Borrar</th>
 
-                </tr>
-            </thead>
-            <tbody>
-                {
-                cart.length > 0 ? cart.map (item =>
-                <tr>
-                <td>{item.cantidad}</td>
-                <td>{item.nombre}</td>
-                <td>{item.precio} €</td>
-                <td>{item.cantidad * parseInt(item.precio)} € </td>
-                <td><Button variant="outline-danger" onClick={() => removeCart(item)} >X</Button></td>
-                </tr>)
-                :
-                <tr>
-                <td>vacio</td>
-                <td>vacio</td>
-                <td>vacio</td>
-                <td>vacio</td>
-                <td><Button variant="outline-danger" disabled >X</Button></td>
-                </tr>
-                }
-            </tbody>
-            <thead>
-                <tr>
-                
-                <th>{cantidadTotal()}</th>
-                <th>-</th>
-                <th>Total</th>
-                <th>{valorTotal()} €</th>
-                <th>-</th>
-                
-                </tr>
-            </thead>
-        </Table>
-      </Container>
-      <Container>
-
-        <p>Pone tus datos para finalizar la compra</p>
-        <Form>
-            
-            <input placeholder={"Nombre y apellido"}  type={"text"} value={name}
-            onChange={(e) => {setName(e.currentTarget.value);}}/>
-
-            <input placeholder={"E-mail"} type={"email"} value={email}
-            onChange={(e) => {setEmail(e.currentTarget.value);}}/>
-
-            <input placeholder={"Numero de Celular"} type={"number"} value={phone}
-            onChange={(e) => {setPhone(e.currentTarget.value);}}/>
-        </Form>
-      </Container>
-                    <Form className="d-flex">
-                        <Button variant="outline-success" onClick={terminarCompra}>Finalizar Comprar</Button>
-                    </Form>
-        </>
+    </>
     )
    
    
